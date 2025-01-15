193f124e425c3af4b93d3a95496b3a4a6fc3defe
Dhyanesh Janardan Dharmik <dharmikdj@rknec.edu>
2025-01-15 17:57:28 +0530

    thrid commit by dhyanesh

--- Backend/index.js
+++ Backend/index.js
@@ -58,7 +58,6 @@ const patientschema = new mongoose.Schema({
 
 const patient = mongoose.model('Patient', patientschema);
 
-let token_no=1;
 
 // Start the server
 const PORT = 2000;
@@ -106,8 +105,33 @@ app.post('/add-staff',async(req,res)=>{
 
 app.post('/add-patient',async(req,res)=>{
   try{
-
+    const {doc_id,staff_id,patient_name,patient_desc,mobile_no} = await req.body;
+    const alldoccount = await patient.countDocuments()
+    if(alldoccount==0){
+      const pat = new patient({
+        doc_id:doc_id,
+        staff_id:staff_id,
+        mobile_no:mobile_no,
+        patient_name:patient_name,
+        patient_desc:patient_desc,
+        token_number:1
+      })
+      await pat.save()
+    }else{
+      alldoccount++;
+    const pat = new patient({
+      doc_id:doc_id,
+      staff_id:staff_id,
+      mobile_no:mobile_no,
+      patient_name:patient_name,
+      patient_desc:patient_desc,
+      token_number:alldoccount
+    })
+    await pat.save()
+  }
+    res.status(200).send("Patient Successfully added...")
   }catch(err){
-
+    res.status(500).send(err)
   }
 })
+
